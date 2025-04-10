56fcbe5075cdba6c12bc5aef7a0743bc3f1a893b
Anthony Rocha <anthony.rocha@amd.com>
2025-04-09 17:21:05 -0700

    Initial commit.

---
+++ crate1/src/lib.rs
@@ -0,0 +1,13 @@
+
+/// Adds two numbers together.
+///
+/// # Examples
+///
+/// ```
+/// let result = crate1::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+    
---
+++ crate2/src/lib.rs
@@ -0,0 +1,12 @@
+
+/// Subtracts the second number from the first.
+///
+/// # Examples
+///
+/// ```
+/// let result = crate1::subtract(5, 3);
+/// assert_eq!(result, 2);
+/// ```
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}    
